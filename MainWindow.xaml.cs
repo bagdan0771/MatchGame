e25992571e948028b81c39e95c46d9aa9246c71f
Bogdan Lazorskiy <lazorskiy07@icloud.com>
2023-02-12 23:32:11 +0300

    Examples

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -38,7 +38,7 @@ namespace MatchGame
                 "🐭", "🐭",
                 "🐬", "🐬",
                 "🙈", "🙈",
-                "🦛", "🦛",
+                "🦛", "🦛",//
             };
 
             Random random = new Random();
@@ -51,5 +51,33 @@ namespace MatchGame
                 animalEmoji.RemoveAt(index);
             }
         }
+
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            } //pull
+
+        }
+
+       
     }
 }
